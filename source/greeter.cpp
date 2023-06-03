5c8fea44fceaa5a205a4a4a01be13b71371ddcab
Nicolas IRAGNE <nicoragne@hotmail.fr>
2023-06-03 22:58:24 +0200

    Apply clang-format

--- source/greeter.cpp
+++ source/greeter.cpp
@@ -3,18 +3,23 @@
 
 using namespace greeter;
 
-Greeter::Greeter(std::string _name) : name(std::move(_name)) {}
+Greeter::Greeter(std::string _name)
+    : name(std::move(_name))
+{
+}
 
-std::string Greeter::greet(LanguageCode lang) const {
-  switch (lang) {
+std::string Greeter::greet(LanguageCode lang) const
+{
+    switch (lang)
+    {
     default:
     case LanguageCode::EN:
-      return fmt::format("Hello, {}!", name);
+        return fmt::format("Hello, {}!", name);
     case LanguageCode::DE:
-      return fmt::format("Hallo {}!", name);
+        return fmt::format("Hallo {}!", name);
     case LanguageCode::ES:
-      return fmt::format("¡Hola {}!", name);
+        return fmt::format("¡Hola {}!", name);
     case LanguageCode::FR:
-      return fmt::format("Bonjour {}!", name);
-  }
+        return fmt::format("Bonjour {}!", name);
+    }
 }
--- test/source/greeter.cpp
+++ test/source/greeter.cpp
@@ -1,23 +1,24 @@
-#include <gtest/gtest.h>
 #include <greeter/greeter.h>
 #include <greeter/version.h>
+#include <gtest/gtest.h>
+
 
 #include <string>
 
 TEST(Greeter, Simple)
 {
-  using namespace greeter;
+    using namespace greeter;
 
-  Greeter greeter("Tests");
+    Greeter greeter("Tests");
 
-  EXPECT_EQ(greeter.greet(LanguageCode::EN), "Hello, Tests!");
-  EXPECT_EQ(greeter.greet(LanguageCode::DE), "Hallo Tests!");
-  EXPECT_EQ(greeter.greet(LanguageCode::ES), "¡Hola Tests!");
-  EXPECT_EQ(greeter.greet(LanguageCode::FR), "Bonjour Tests!");
+    EXPECT_EQ(greeter.greet(LanguageCode::EN), "Hello, Tests!");
+    EXPECT_EQ(greeter.greet(LanguageCode::DE), "Hallo Tests!");
+    EXPECT_EQ(greeter.greet(LanguageCode::ES), "¡Hola Tests!");
+    EXPECT_EQ(greeter.greet(LanguageCode::FR), "Bonjour Tests!");
 }
 
 TEST(Greeter, Version)
 {
-  static_assert(std::string_view(GREETER_VERSION) == std::string_view("1.0"));
-  EXPECT_EQ(std::string(GREETER_VERSION), std::string("1.0"));
+    static_assert(std::string_view(GREETER_VERSION) == std::string_view("1.0"));
+    EXPECT_EQ(std::string(GREETER_VERSION), std::string("1.0"));
 }
